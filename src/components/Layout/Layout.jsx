0843661be650e2fb2d04e48d71d3437802799a46
Omar husien <139303831+Omarhusien072@users.noreply.github.com>
2026-09-08 17:26:42 +0300

    Removed Footer from layout and puted in only used pages footer will only remain at specific pages and not all so not necessary be found in layout.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,5 @@
 import LayoutStyles from './Layout.module.css';
 import { Outlet } from 'react-router-dom';
-import Footer from '../Footer/Footer'
 import Navbar from '../Navbar/Navbar';
 
 export default function Layout() {
@@ -9,7 +8,6 @@ export default function Layout() {
             <main className='min-h-screen flex flex-col justify-center items-center'>
                 <Navbar />
                     <Outlet />
-                <Footer />
             </main>
         </>
     );
