e4da98b432ff0285891f0ee278761c8c5cd1adac
Omar husien <139303831+Omarhusien072@users.noreply.github.com>
2026-09-08 17:31:14 +0300

    added the footer component there so it will only displayed at home and mylist for now at least

--- src/components/MyList/MyList.jsx
+++ src/components/MyList/MyList.jsx
@@ -1,8 +1,14 @@
+import Footer from "../Footer/Footer";
+
 export default function MyList() {
 
     return (
         <>
-            MyList
+            <div>
+                <h1 className="text-white text-2xl text-center">List</h1>
+                <Footer />
+            </div>
+
         </>
     );
 }
